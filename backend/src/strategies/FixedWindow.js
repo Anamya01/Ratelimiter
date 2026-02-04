94bb815e938ae6d83ed7cb433210ea5dbd9c6d89
Anamya01 <anamya666@gmail.com>
2026-02-04 21:40:56 +0530

    Added Fixed Window Strategy

---
+++ backend/src/strategies/FixedWindow.js
@@ -0,0 +1,35 @@
+const { getRedisClient } = require("../config/redis.config");
+
+class FixedWindow{
+  async allowRequest(key, limit, windowms) {
+    const redis = await getRedisClient();
+    const now = Date.now();
+    
+    const windowStart = Math.floor(now / Window) * windowms;
+    const WindowKey = `Rate:FixedWindow:${key}:${windowStart}`
+    try {
+      const count = await redis.incr(WindowKey);
+      if (count === 1) {
+        await redis.expire(WindowKey, Math.ceil(windowms/1000)+1);
+      }
+      if (count <= limit) {
+        return {
+          allowed: true,
+          remaining: limit - count
+        };
+      }
+      else {
+        return {
+          allowed: false,
+          remaining: 0
+        };
+      }
+    }
+    catch (error) {
+      console.error('Fixed Window Error', error)
+      throw error;
+    }
+  }
+}
+
+module.exports = FixedWindow;
